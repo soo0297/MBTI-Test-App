baa54f6e439ca3e4b03b92f5ea01083dd4a9b763
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-09-09 00:06:01 +0900

    feat:useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,7 @@
-import { useState } from "react";
-import { Route, Router, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Layout from "./components/Layout";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Profile from "./pages/Profile";
-import TestPage from "./pages/TestPage";
-import TestResultPage from "./pages/TestResultPage";
+import Router from "./shared/Router";
 
 const App = () => {
-  const [user, setUser] = useState(null);
-  return (
-    <Router>
-      <Layout user={user} setUser={setUser}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login setUser={setUser} />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute user={user}>
-                <Profile user={user} setUser={setUser} />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/test"
-            element={
-              <ProtectedRoute user={user}>
-                <TestPage user={user} />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/results"
-            element={
-              <ProtectedRoute user={user}>
-                <TestResultPage user={user} />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Layout>
-    </Router>
-  );
+  return <Router />;
 };
 
 export default App;
---
+++ src/shared/Router.jsx
@@ -0,0 +1,54 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "../components/Layout";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import ProtectedRoute from "../components/ProtectedRoute";
+import Profile from "../pages/Profile";
+import TestPage from "../pages/TestPage";
+import TestResultPage from "../pages/TestResultPage";
+import { MbtiTestContext } from "../context/mbtiContext";
+import { useState } from "react";
+
+const Router = () => {
+  const [user, setUser] = useState(null);
+  return (
+    <MbtiTestContext.Provider value={{ user, setUser }}>
+      <Router>
+        <Layout user={user} setUser={setUser}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login setUser={setUser} />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute user={user}>
+                  <Profile user={user} setUser={setUser} />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/test"
+              element={
+                <ProtectedRoute user={user}>
+                  <TestPage user={user} />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/results"
+              element={
+                <ProtectedRoute user={user}>
+                  <TestResultPage user={user} />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Layout>
+      </Router>
+    </MbtiTestContext.Provider>
+  );
+};
+
+export default Router;
